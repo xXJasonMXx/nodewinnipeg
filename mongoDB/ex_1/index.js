de8d2c4e09877a50121445bde05c2c61db90af7c
Jason Monette <jasonmonette44@gmail.com>
2018-08-08 20:29:17 -0600

    started mongoDB

---
+++ mongoDB/ex_1/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const port = 3000;
+
+server.set('viewengine', 'pug')
+
+server.get('/', (req, res) => {
+    MongoClient.connect(url, (err, client) => {
+        console.log(`Connected to the server!`)
+        const db = client.db('comics')
+        const collection = db.collection('superheroes');
+
+        collection.find({}).toArray((err, documents) => {
+            res.render('index', {documents: documents });
+            client.close();
+        })
+    })
+})
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log(`there is an error ${err}`)
+    }
+    console.log(`Running on port: ${port}`)
+})
